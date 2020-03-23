290321f80daabd7e7311e80b4788a27ca3b4476e
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-03-22 19:22:28 -0600

    Hooks and contexts Also Home and Not found pages!

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ searchUsers, clearUsers, showClear, setAlert }) => {
+const Search = () => {
 	const [text, setText] = useState('');
+	const githubContext = useContext(GithubContext);
+	const { users, clearUsers, searchUsers } = githubContext;
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
 
 	const onSubmit = e => {
 		e.preventDefault();
@@ -34,20 +39,13 @@ const Search = ({ searchUsers, clearUsers, showClear, setAlert }) => {
 					className='btn btn-dark btn-block'
 				/>
 			</form>
-			{showClear && (
+			{users.length > 0 && (
 				<button className='btn btn-light btn-block' onClick={clearUsers}>
 					Clear
 				</button>
 			)}
 		</div>
 	);
-};
-
-Search.propTypes = {
-	searchUsers: PropTypes.func.isRequired,
-	clearUsers: PropTypes.func.isRequired,
-	showClear: PropTypes.bool.isRequired,
-	setAlert: PropTypes.func.isRequired
 };
 
 export default Search;
